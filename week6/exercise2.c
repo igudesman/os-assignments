7ff46dd87b6526688c48b7266cfb04807d44e342
Daniil Igudesman <digudesm@gmail.com>
2020-09-23 20:10:14 +0300

    Update exercise2.c

--- week6/exercise2.c
+++ week6/exercise2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 struct Process {
+  int id;
   int burst_time;
   int arrival_time;
   int waiting_time;
   int completion_time;
   int turn_around_time;
+  bool is_finished;
 };
 
 void find_waiting_time(struct Process *arr, int size) {
@@ -17,7 +20,7 @@ void find_waiting_time(struct Process *arr, int size) {
   }
 
   for (int i = 0; i < size; i++) {
-    printf("Waiting time of %d process: %d\n", i, (arr+i)->waiting_time);
+    printf("Waiting time of %d process: %d\n", arr[i].id, (arr+i)->waiting_time);
   }
 }
 
@@ -26,46 +29,62 @@ void completion_time(struct Process *arr, int size) {
   printf("The completion time of 0 process: %d\n", arr->completion_time);
   for (int i = 1; i < size; i++) {
     int temp = MAX((arr+i-1)->completion_time + (arr+i)->burst_time, (arr+i)->arrival_time + (arr+i)->burst_time);
-    printf("The completion time of %d process: %d\n", i, temp);
+    printf("The completion time of %d process: %d\n", arr[i].id, temp);
     (arr+i)->completion_time = temp;
   }
 }
 
-void sort_by_burst_time(struct Process *arr, int size) {
-  for (int i = 1; i < size; i++) {
-    for (int j = 0; j < size - i; j++) {
-      if ((arr + j)->burst_time - (arr + j + 1)->burst_time > 0) {
-        struct Process temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j + 1] = temp;
+void sort_jobs(struct Process *arr, int size) {
+  int current_time = 0;
+  struct Process next;
+  for (int i = 0; i < size - 1; i++) {
+    bool is_init = false;
+    struct Process next; 
+    struct Process with_min_arrival_time;
+    int temp_index;
+    int index = i;
+    bool is_found = false;
+    for (int j = i; j < size; j++) {
+      if ((arr + j)->arrival_time <= current_time) {
+        is_found = true;
+        if (!is_init) {
+          next = arr[j];
+          index = j;
+          is_init = true;
+        } else if (arr[j].burst_time < next.burst_time) {
+          next = arr[j];
+          index = j;
+        }
+      } else {
+        if (arr[j].arrival_time < with_min_arrival_time.arrival_time) {
+          with_min_arrival_time = arr[j];
+          temp_index = j;
+        }
       }
     }
-  }
-}
-
-void sort_by_arrival_time(struct Process *arr, int size) {
-  for (int i = 1; i < size; i++) {
-    for (int j = 0; j < size - i; j++) {
-      if ((arr + j)->arrival_time - (arr + j + 1)->arrival_time > 0) {
-        struct Process temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j + 1] = temp;
-      }
+    if (!is_found) {
+      next = with_min_arrival_time;
+      index = temp_index;
+      current_time += with_min_arrival_time.arrival_time - current_time;
     }
+    struct Process temp = arr[i];
+    arr[i] = arr[index];
+    arr[index] = temp;
+    current_time += (arr + i)->burst_time;
   }
 }
 
 void turn_around_time(struct Process *arr, int size) {
   for (int i = 0; i < size; i++) {
     int temp = (arr+i)->waiting_time + (arr+i)->burst_time;
-    printf("The turn around time of %d process: %d\n", i, temp);
+    printf("The turn around time of %d process: %d\n", arr[i].id, temp);
     (arr+i)->turn_around_time = temp;
   }
 }
 
 void print_processes(struct Process *arr, int size) {
   for (int i = 0; i < size; i++) {
-    printf("Burst: %d, Arrival: %d\n", (arr + i)->burst_time, (arr + i)->arrival_time);
+    printf("id: %d\n", (arr + i)->id);
   }
 }
 
@@ -98,18 +117,18 @@ int main(void) {
   for (int i = 0; i < n; i++) {
     int burst_time;
     int arrival_time;
-    printf("Indicate the burst time of %d process: ", i);
-    scanf("%d", &burst_time);
     printf("Indicate the arrival time of %d process: ", i);
     scanf("%d", &arrival_time);
+    printf("Indicate the burst time of %d process: ", i);
+    scanf("%d", &burst_time);
     printf("\n");
     
     processes[i].burst_time = burst_time;
     processes[i].arrival_time = arrival_time;
+    processes[i].id = i;
   }
 
-  sort_by_burst_time(processes, n);
-  sort_by_arrival_time(processes, n);
+  sort_jobs(processes, n);
 
   find_waiting_time(processes, n);
   completion_time(processes, n);
